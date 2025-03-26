fff25f84f654ef896ef57c89ca3abbe0c5dbd5de
이석주 <nick981213@naver.com>
2025-03-26 15:14:58 +0900

    - feat: first commit

---
+++ src/main/java/com/capstone/HisTour/HisTourApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.HisTour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HisTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HisTourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/HisTour/HisTourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.HisTour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisTourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
