b4d1816c1cfc152e080722b11d963c262029ce30
이석주 <nick981213@naver.com>
2025-07-06 23:53:15 +0900

    fix: heritageServiceTest 주석 처리

--- src/test/java/com/capstone/HisTour/heritage/HeritageServiceTest.java
+++ src/test/java/com/capstone/HisTour/heritage/HeritageServiceTest.java
@@ -1,23 +1,23 @@
-package com.capstone.HisTour.heritage;
-
-import com.capstone.HisTour.domain.heritage.repository.HeritageRepository;
-import com.capstone.HisTour.domain.heritage.service.HeritageService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-public class HeritageServiceTest {
-
-    HeritageService heritageService;
-
-    @MockitoBean
-    HeritageRepository heritageRepository;
-
-//    @BeforeEach
-//    void setUp() {
-//        heritageService = new HeritageService(heritageRepository);
-//    }
-
-}
+//package com.capstone.HisTour.heritage;
+//
+//import com.capstone.HisTour.domain.heritage.repository.HeritageRepository;
+//import com.capstone.HisTour.domain.heritage.service.HeritageService;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.springframework.test.context.bean.override.mockito.MockitoBean;
+//import org.springframework.test.context.junit.jupiter.SpringExtension;
+//
+//@ExtendWith(SpringExtension.class)
+//public class HeritageServiceTest {
+//
+//    HeritageService heritageService;
+//
+//    @MockitoBean
+//    HeritageRepository heritageRepository;
+//
+////    @BeforeEach
+////    void setUp() {
+////        heritageService = new HeritageService(heritageRepository);
+////    }
+//
+//}
